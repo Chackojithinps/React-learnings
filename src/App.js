a3dba9328df101e1206107ba14d76e93c8f2f4fb
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-06-21 11:35:59 +0530

    if condition

--- src/App.js
+++ src/App.js
@@ -2,23 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Job name='jitin' age={24} place='kannur'/>
-    </div>
-  );
+   const age=116;
+   if(age>=18){
+    return <h1>Verified</h1>
+   }else{
+    return <h1>Not Verified</h1>
+   }
 }
 
-const name2="kiran"
-
-function Job(props){
-  return(
-    <div>
-       <h1>{name2}</h1>
-       <p>{props.age}</p>
-       <p>{props.place}</p>
-    </div>
-  )
-}
 
 export default App;
