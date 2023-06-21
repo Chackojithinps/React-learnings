947279fccee1b38bfee93637ce885e68cc7cb01a
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-06-21 12:19:21 +0530

    map function

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-   const age=116;
-   const isColor=true;
+   const names=["jithin","arun","kiran"]
    return (
      <div className='App'>
-        {age>=18?<p1>over age</p1>:<p>under Age</p>}
-        <h1 style={{color:isColor?"green":"red"}}>Jithin</h1>
-
-        {isColor && <button>This is a button</button>}
+         {names.map((val,key)=>{
+            return <h1 key={key}>{val}</h1>
+         })}
      </div>
    )
 }
