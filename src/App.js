495e2882805564d2382e1dda326490a61745c058
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-06-21 15:27:27 +0530

    color change by click

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import './App.css';
 // import {Planets} from './User'
 import {useState} from 'react'
 function App() {
-  const [state,setState]= useState(false)
+  const [color,setcolor]= useState("black")
    return (
-    
-     <div className='App'>
-         {/* <Planets text={text}/> */}
+       <div className='App'>
          <button onClick={()=>{
-           setState(!state)
-         }}>Show/Hidden</button>
-         {state && <h1>Hello ! This is jithin chacko</h1> }
+           setcolor(color==="black" ? "red" : "black")
+         }}>change color</button>
+        
+      <h1 style={{color:color}}>Hello! This is Jithin chacko </h1>
      </div>
    )
 }
