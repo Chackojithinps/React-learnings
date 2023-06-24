f82072a6ad4b190024f794f31321521b43355a8f
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-06-24 10:04:30 +0530

    changes

--- src/Todo.js
+++ src/Todo.js
@@ -1,5 +1,5 @@
 export const Task=(props)=>{
-    return <div style={{color:props.completed?"green":"red "}}>
+    return <div style={{color:props.completed?"green":"red"}}>
     <h1>{props.taskName}</h1>
     <button onClick={()=>props.deleteItem(props.id)}>X</button>
     <button onClick={()=>props.completeItem(props.id)}>Complete</button>
