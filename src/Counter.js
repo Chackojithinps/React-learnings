9b7773aa57e63eecf66f7b90ca8996a197814b2f
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-06-27 19:10:35 +0530

    notes and fragments

--- src/Counter.js
+++ src/Counter.js
@@ -3,10 +3,10 @@ import { useState } from "react"
 export const Counter=()=>{
     const [count,setCount]=useState(0)
     return (
-        <div>
+        <>
         <button onClick={()=>setCount(count+1)}>Increment</button>
         <h1>This is component Counter : {count} </h1>
 
-        </div>
+        </>
     )
 }
