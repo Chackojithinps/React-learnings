6add501ddf7dd60d57989de6ca3dc7626155ab3c
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-06-27 15:41:06 +0530

    props destructring and using

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
 import './App.css';
 // import {Planets} from './User'
 import {useState} from 'react'
-import { Task } from './Todo';
+import { Count } from './count';
+// import { Task } from './Todo';
 function App() {
-   const [newTask,setNewtask]=useState("")
-   const [Todo,setTodo]=useState([])
-   const newItems=(event)=>{
-      setNewtask(event.target.value)
-   }
-   const addItems=()=>{
-      const task={
-         id:Todo.length===0?1:Todo[Todo.length-1].id+1,
-         taskName:newTask,
-         completed:false
-      }
-      setTodo([...Todo,task])
-   }
-   const deleteItem = (newId) => {
-    var newTodLists = Todo.filter((task) =>{
-      return task.id!==newId
-    })
+   // const [newTask,setNewtask]=useState("")
+   // const [Todo,setTodo]=useState([])
+   const [count,setCount]=useState(0)
+   // const newItems=(event)=>{
+   //    setNewtask(event.target.value)
+   // }
+//    const addItems=()=>{
+//       const task={
+//          id:Todo.length===0?1:Todo[Todo.length-1].id+1,
+//          taskName:newTask,
+//          completed:false
+//       }
+//       setTodo([...Todo,task])
+//    }
+//    const deleteItem = (newId) => {
+//     var newTodLists = Todo.filter((task) =>{
+//       return task.id!==newId
+//     })
     
-    setTodo(newTodLists)
-  };
-  const completeItem=(newId)=>{
-   setTodo(
-      Todo.map((task)=>{
-         if(task.id===newId){
-            return {...task,completed:true}
-         }else{
-            return task
-         }
-      })
-   )
+//     setTodo(newTodLists)
+//   };
+//   const completeItem=(newId)=>{
+//    setTodo(
+//       Todo.map((task)=>{
+//          if(task.id===newId){
+//             return {...task,completed:true}
+//          }else{
+//             return task
+//          }
+//       })
+//    )
+//  }
+ const addCount=()=>{
+     setCount(count+1)
  }
    return (
        <div className='App'>
-         <div>
+         {/* <div>
             <input onChange={newItems} text/>
             <button onClick={addItems}>Add Items</button>
          </div>
@@ -44,7 +49,10 @@ function App() {
              {Todo.map((item,key)=>{
                    return <Task taskName={item.taskName} id={item.id} deleteItem={deleteItem} completeItem={completeItem} />
              })}
-         </div>
+         </div> */}
+         <button onClick={addCount}>Add count</button>
+         <Count title="First counter" count={count}/>
+         <Count title="Second counter" count={count}/>
        </div>
    )
 }
---
+++ src/count.js
@@ -0,0 +1,11 @@
+export const Count=(props)=>{
+    const {title,count}=props
+    return(
+        
+        <div>
+           <h1>{title}</h1>
+           <h1>{count}</h1>
+
+        </div>
+    )
+}
