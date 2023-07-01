7795535e5caf91d8ab42fd5575ffb97ee2acd3a5
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-07-02 00:51:28 +0530

    Routes in react

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
 import axios from 'axios'
 import React from 'react'
-import { useState } from 'react'
+import Footer from './Components/Layouts/Footer'
+import Header from './Components/Layouts/Header'
+import Home from './Components/Pages/Home'
+import About from './Components/Pages/About'
+import Contact from './Components/Pages/Contact'
+import Error from './Components/Layouts/Error'
+import {
+  BrowserRouter , Routes,
+  Route,
+  Link,
+} from 'react-router-dom'
  export const App = () => {
-    const [state,setState]= useState([])
-    const [bool,setBool]=useState(false)
    
   return (
     <div>
-        <h1>Hello world</h1>
-        <button onClick={()=>{
-            setBool(true)
-            axios.get('https://jsonplaceholder.typicode.com/posts').then((res)=>{
-               console.log(res)
-               setState(res.data)
-               setBool(false)
-               
-            })
-        }}>Generate API</button>
-        {    
-             bool!==true?
-             state.map((item,index)=>
-                <div>
-                 <h1>{index+1}</h1>
-                 <h4>{item.title}</h4>
-                </div>
-            ):"Loading......"
-        }
+        <Header/>
+        <BrowserRouter>
+            <ul>
+              <li>
+                <Link to='/'>HOME</Link>
+              </li>
+              <li>
+                <Link to='/about'>About</Link>
+              </li>
+              <li>
+                <Link to='/contact'>Contact</Link>
+              </li>
+            </ul>
+            <Routes>
+               <Route exact path='/' element={<Home/>} />
+               <Route path='about' element={<About/>}/>
+              
+               {/* <Route path='about'>
+                  <About/>
+               </Route>
+               <Route path='contact'>
+                  <Contact/>
+                
+               </Route> */}
+               <Route path='contact' element={<Contact/>}></Route>
+            </Routes>
+        </BrowserRouter>
+       
+        <Footer/>
+        
     </div>
   )
 }
