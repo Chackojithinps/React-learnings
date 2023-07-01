b50cdbb572e242e1513086a99013cf386d861721
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-07-02 00:53:02 +0530

    Learining Context api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import axios from 'axios'
 import React from 'react'
 import Footer from './Components/Layouts/Footer'
-import Header from './Components/Layouts/Header'
-import Home from './Components/Pages/Home'
-import About from './Components/Pages/About'
-import Contact from './Components/Pages/Contact'
-import Error from './Components/Layouts/Error'
+
 import {
   BrowserRouter , Routes,
   Route,
@@ -15,35 +11,7 @@ import {
    
   return (
     <div>
-        <Header/>
-        <BrowserRouter>
-            <ul>
-              <li>
-                <Link to='/'>HOME</Link>
-              </li>
-              <li>
-                <Link to='/about'>About</Link>
-              </li>
-              <li>
-                <Link to='/contact'>Contact</Link>
-              </li>
-            </ul>
-            <Routes>
-               <Route exact path='/' element={<Home/>} />
-               <Route path='about' element={<About/>}/>
-              
-               {/* <Route path='about'>
-                  <About/>
-               </Route>
-               <Route path='contact'>
-                  <Contact/>
-                
-               </Route> */}
-               <Route path='contact' element={<Contact/>}></Route>
-            </Routes>
-        </BrowserRouter>
-       
-        <Footer/>
+        
         
     </div>
   )
